1242179abe3a35bd763817a7701dfa6fedbcb394
diogo varaschin <diogovaraschin@outlook.com>
2025-03-14 15:23:49 -0300

    exercicios 1, 2 e 3

--- Raciocinio Computacional/semana 1/exer01.py
+++ Raciocinio Computacional/semana 1/exer01.py
@@ -1,3 +1,7 @@
+# Crie um algoritmo que solicita ao usuário que insira seu nome e exibe na tela uma mensagem de saudação
+
+# Exemplo: um usuário digita o seu nome (como Fulano de Tal). O algoritmo deve retornar uma mensagem chamada “Oi, Fulano de Tal! Espero que você tenha uma boa semana!”.
+
 nome = input("digite seu nome: ")
 
 print(f"olá {nome}, como vai?")
---
+++ Raciocinio Computacional/semana 1/exer02.py
@@ -0,0 +1,8 @@
+# Crie um algoritmo que solicita ao usuário que insira um número decimal e exibe o dobro desse número na tela.
+
+# Exemplo: um usuário digita o número 23.51. O algoritmo deve retornar 47.02.
+
+num = float(input("insira um valor decimal: "))
+dobro = num*2
+
+print(f"resultado: {dobro}")
