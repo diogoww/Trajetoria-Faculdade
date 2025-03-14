88ef216b5b4eccfafbd48a3569430942f6d0a347
diogo varaschin <diogovaraschin@outlook.com>
2025-03-14 15:31:51 -0300

    exercicio 5

---
+++ Raciocinio Computacional/semana 1/exer05.py
@@ -0,0 +1,7 @@
+# 5. Crie um algoritmo que solicita ao usuário que insira a temperatura em graus Celsius e exibe na tela a temperatura em Fahrenheit
+
+tempCelsius = float(input("digite a temperatura em graus celsius: "))
+
+tempFahrenheit = (tempCelsius*9/5)+32
+
+print(f"a temperatura em fahrenheit eh: {tempFahrenheit}")
