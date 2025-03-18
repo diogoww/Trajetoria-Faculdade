64d9c96e72e6fa44df71ca896beb63d9613316ad
diogo varaschin <diogovaraschin@outlook.com>
2025-03-18 17:14:30 -0300

    Raciocinio Computacional -  Atividade Formativa Semana 2 | Concluida

---
+++ Raciocinio Computacional/Sistema PUC/main.py
@@ -0,0 +1,36 @@
+print("---- MENU PRINCIPAL ----")
+print("(1) Gerenciar Estudantes.")
+print("(2) Gerenciar Professores.")
+print("(3) Gerenciar Disciplinas.")
+print("(4) Gerenciar Turmas.")
+print("(5) Gerenciar Matrículas.")
+print("(0) Sair.")
+opcao = int(input("Informe a opção desejada: "))
+
+if opcao >= 1 and opcao <= 5:
+    print(f"Você escolheu a opção {opcao}.")
+    print("\n")
+
+    print(f"Menu de Operações - Opção {opcao}")
+    print("(1) Incluir.")
+    print("(2) Listar.")
+    print("(3) Atualizar.")
+    print("(4) Excluir.")
+    print("(0) Voltar ao Menu Principal.")
+
+    opcaoSec = int(input("Informe a opção desejada: "))
+    print(f"Você escolheu a opção {opcaoSec}.")
+    if opcaoSec >= 0 and opcaoSec <= 4:
+        print(f"Você escolheu a opção {opcaoSec}.")
+    else:
+        print("Escolha uma opção VÁLIDA")
+
+    print("\n")
+    print("==== ATUALIZAÇÃO ====")
+    print("Finalizando aplicação...")
+elif opcao == 0:
+    print("\n")
+    print("Saindo...")
+else:
+    print("Escolha uma opção **VÁLIDA**")
+
