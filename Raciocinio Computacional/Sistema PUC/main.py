54a2ff768f53d8a1dc9ccaaae587b48c467aeee4
diogo varaschin <diogovaraschin@outlook.com>
2025-04-06 14:36:58 -0300

    semana 04 - Atividade Somativa concluida, add funcionalidades ao projeto 'Sistema PUC'

--- Raciocinio Computacional/Sistema PUC/main.py
+++ Raciocinio Computacional/Sistema PUC/main.py
@@ -1,3 +1,4 @@
+nomesEstudantes = []
 while True:
     print("---- MENU PRINCIPAL ----")
     print("(1) Gerenciar Estudantes.")
@@ -8,35 +9,51 @@ while True:
     print("(0) Sair.")
     opcao = int(input("Informe a opção desejada: "))
 
-    if opcao >= 1 and opcao <= 5:
-        while True:
-            print(f"Você escolheu a opção {opcao}.")
-            print("\n")
 
-            print(f"Menu de Operações - Opção {opcao}")
-            print("(1) Incluir.")
-            print("(2) Listar.")
-            print("(3) Atualizar.")
-            print("(4) Excluir.")
-            print("(0) Voltar ao Menu Principal.")
-
-            opcaoSec = int(input("Informe a opção desejada: "))
-            print(f"Você escolheu a opção {opcaoSec}.")
-            if opcaoSec >= 1 and opcaoSec <= 4:
-                print(f"Você escolheu a opção {opcaoSec}.")
-            elif opcaoSec == 0:
+    if opcao == 1:
+            while True:
+                print(f"Você escolheu a opção {opcao}.")
                 print("\n")
-                print("Voltando ao menu principal...")
-                break
-            else:
-                print("Escolha uma opção VÁLIDA")
 
-            print("\n")
-            print("==== ATUALIZAÇÃO ====")
-            print("Finalizando aplicação...")
+                print(f"Menu de Operações - Opção {opcao}")
+                print("(1) Incluir.")
+                print("(2) Listar.")
+                print("(3) Atualizar.")
+                print("(4) Excluir.")
+                print("(0) Voltar ao Menu Principal.")
+
+                opcaoSec = int(input("Informe a opção desejada: "))
+                print(f"Você escolheu a opção {opcaoSec}.")
+
+                if opcaoSec == 1:
+                    print("OPÇÃO 1 - INCLUIR ESTUDANTES")
+                    nome = input("Digite o nome do Estudante: ")
+                    nomesEstudantes.append(nome)
+                elif opcaoSec == 2:
+                    print("OPÇÃO 2 - LISTAR ESTUDANTES")
+                    if not nomesEstudantes:
+                            print("Não há estudantes cadastrados")
+                    for nome in nomesEstudantes:
+                        print(nome)
+                elif opcaoSec == 3:
+                    print("EM DESENVOLVIMENTO...")
+                elif opcaoSec == 4:
+                    print("EM DESENVOLVIMENTO...")
+                elif opcaoSec == 0:
+                    print("\n")
+                    print("Voltando ao menu principal...")
+                    break
+                else:
+                    print("Escolha uma opção VÁLIDA")
+                print("\n")
+    elif opcao >= 2 and opcao <=5:
+          print("EM DESENVOLVIMENTO...")
+          print("Voltando ao menu principal...")
+          print("\n")
     elif opcao == 0:
-        print("\n")
-        print("Saindo...")
-        break
+                print("\n")
+                print("Saindo...")
+                print("Finalizando aplicação...")
+                break
     else:
-        print("Escolha uma opção **VÁLIDA**")
+                print("Escolha uma opção **VÁLIDA**")
