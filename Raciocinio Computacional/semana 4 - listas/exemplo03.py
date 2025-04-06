09f4ad8ae216c5361cd2489fde36beb038b96798
diogo varaschin <diogovaraschin@outlook.com>
2025-04-06 10:30:06 -0300

    semana 4 - exemplo 03

---
+++ Raciocinio Computacional/semana 4 - listas/exemplo03.py
@@ -0,0 +1,4 @@
+# Dada a lista de dados nums = [1, 4, 23, 11, 8], corra a lista usando um objeto range e imprima cada elemento em uma linha
+num = [1, 4, 23, 11, 8]
+for i in range(len(num)):
+    print(num[i])
