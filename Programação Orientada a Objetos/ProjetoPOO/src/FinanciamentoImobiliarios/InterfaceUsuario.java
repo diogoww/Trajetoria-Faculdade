b2da6e8a64dff5eb07985efa5a6e59ebc6ce79cb
Diogo Varaschin <diogovaraschin@outlook.com>
2025-06-11 09:21:48 -0300

    semana 02 POO Ativ. Formativa | small changes in InterfaceUsuario

--- Programação Orientada a Objetos/ProjetoPOO/src/FinanciamentoImobiliarios/InterfaceUsuario.java
+++ Programação Orientada a Objetos/ProjetoPOO/src/FinanciamentoImobiliarios/InterfaceUsuario.java
@@ -4,24 +4,37 @@ import java.util.Scanner;
 
 public class InterfaceUsuario {
 
-    Scanner scanner = new Scanner(System.in);
+    private Scanner scanner;
 
-    public void valorImovel(){
-        System.out.print("digite o valor do imovel: ");
-        double valorImovel = scanner.nextDouble();
-        System.out.println("o valor eh: " + valorImovel);
+    public InterfaceUsuario(){
+        this.scanner = new Scanner(System.in);
     }
 
-    public void prazoFinanciamento() {
+    public double valorImovel(){
+        System.out.print("digite o valor do imovel: ");
+        double valor = scanner.nextDouble();
+        System.out.println("o valor eh: " + valor);
+        return valor;
+    }
+
+    public int prazoFinanciamento() {
         System.out.print("digite o prazo do financiamento (em anos): ");
         int prazo = scanner.nextInt();
         System.out.println("o prazo eh de: " + prazo + " anos");
+        return prazo;
     }
 
-    public void taxaJuros() {
+    public double taxaJuros() {
         System.out.print("digite a taxa de juros anual: ");
         double taxas = scanner.nextDouble();
         System.out.println("a taxa de juros eh: " + taxas);
+        return taxas;
+    }
+
+    public void fecharScanner(){
+        if (scanner != null){
+            scanner.close();
+        }
     }
 
 }
