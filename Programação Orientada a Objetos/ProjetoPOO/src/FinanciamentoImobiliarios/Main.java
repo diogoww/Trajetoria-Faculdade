0773d2b9e8b141bb1675ecdeb0a3099229ab46b5
Diogo Varaschin <diogovaraschin@outlook.com>
2025-06-11 09:25:46 -0300

    semana 02 POO Ativ. Formativa | implement features in Main.java

--- Programação Orientada a Objetos/ProjetoPOO/src/FinanciamentoImobiliarios/Main.java
+++ Programação Orientada a Objetos/ProjetoPOO/src/FinanciamentoImobiliarios/Main.java
@@ -2,4 +2,14 @@ package FinanciamentoImobiliarios;
 
 public class Main {
 
+    public static void main(String [] args){
+
+        InterfaceUsuario interfaceUsuario = new InterfaceUsuario();
+
+        int prazoDigitado = interfaceUsuario.prazoFinanciamento();
+        double valorImovelDigitado = interfaceUsuario.valorImovel();
+        double taxaDigitada = interfaceUsuario.taxaJuros();
+        
+        interfaceUsuario.fecharScanner();
+    }
 }
