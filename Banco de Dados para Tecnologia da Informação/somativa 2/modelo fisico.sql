0fc10b5d5ad537da33420eca6f4bc825535b3b51
Diogo Varaschin <diogovaraschin@outlook.com>
2025-09-29 15:14:32 -0300

    Atividade Somativa 2 -  Modelo Físico

---
+++ Banco de Dados para Tecnologia da Informação/somativa 2/modelo fisico.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE VinhosDoSul;
+USE VinhosDoSul;
+
+CREATE TABLE Vinicola(
+	codVinicola bigint auto_increment primary key,
+    nomeVinicola varchar(100) not null,
+    descricaoVinicola text, 
+    foneVinicola varchar(15),
+    emailVinicola varchar(15),
+    foreign key (codRegiao) references Regiao(codRegiao)
+);
+
+CREATE TABLE Regiao(
+	codRegiao bigint auto_increment primary key,
+    nomeRegiao varchar(100) not null,
+    descricaoRegiao text
+);
+
+CREATE TABLE Vinho(
+	codVinho bigint auto_increment primary key,
+    nomeVinho varchar(50) not null,
+    tipoVinho varchar(30) not null,
+    anoVinho int not null,
+    descricaoVinho text,
+    foreign key (codVinicola) references Vinicola(codVinicola)
+);
